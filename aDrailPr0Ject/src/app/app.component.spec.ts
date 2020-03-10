99148a1b8edea0fa4ac1f2042a7f58023082c74d
Jyoti Talukdar <n.jyoti.talukdar@accenture.com>
2020-03-10 19:08:36 +0530

    Adrain Projet

--- aDrailPr0Ject/src/app/app.component.spec.ts
+++ aDrailPr0Ject/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'adrain'`, () => {
+  it(`should have as title 'aDrailPr0Ject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('adrain');
+    expect(app.title).toEqual('aDrailPr0Ject');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to adrain!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to aDrailPr0Ject!');
   });
 });
